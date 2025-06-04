3125b77a7e7a22f966cdc2c0527c9d561d88826b
Pranav Rajan <rajan.pranav320@gmail.com>
2025-06-04 21:32:31 +0200

    fix config file

--- svelte.config.js
+++ svelte.config.js
@@ -5,26 +5,16 @@ export default {
     adapter: adapter({
       pages: "build",
       assets: "build",
-      fallback: undefined,
+      fallback: "index.html",
       precompress: false,
-      strict: true,
+      strict: false,
     }),
     paths: {
       base: process.argv.includes("dev") ? "" : "/idyll",
     },
+    prerender: {
+      handleHttpError: "warn",
+      handleMissingId: "warn",
+    },
   },
 };
-
-// import adapter from '@sveltejs/adapter-auto';
-
-// /** @type {import('@sveltejs/kit').Config} */
-// const config = {
-// 	kit: {
-// 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-// 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-// 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-// 		adapter: adapter()
-// 	}
-// };
-
-// export default config;
